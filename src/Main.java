27b85bb9fb7f402b0b1d198498e1fe749be8fa6e
MoonLightt01 <byrman.slavik@gmail.com>
2023-05-30 22:42:25 +0300

    hw7

---
+++ src/Main.java
@@ -0,0 +1,61 @@
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // thw first lesson
+
+
+//        int s = 1;
+//        int l = 2;
+//        int catCount = 3;
+//        int dogCount = 4;
+//        int butterflyCount = 5;
+//
+//        String a = "Hello";
+//        char one = '1';
+//        char two = '2';
+//        int test = 10;
+//        double b = 1.5;
+//        System.out.println(s);
+//        System.out.println(l);
+//        System.out.println(catCount);
+//        System.out.println(dogCount);
+//        System.out.println(butterflyCount);
+//        System.out.println(a);
+//        System.out.println(one);
+//        System.out.println(two);
+//        System.out.println(test);
+//        System.out.println(b);
+
+
+//        System.out.println("\t\tДобро пожаловать в калькулятор Мунлайта\n");
+//        System.out.println("Тут мы умножаем)");
+//        System.out.println("Введите первое число:");
+//        double a = new Scanner(System.in).nextDouble();
+//        System.out.println("Введите второе число:");
+//        double b = new Scanner(System.in).nextDouble();
+//        System.out.println(a + " * " + b + " = " + (a*b));
+
+//        System.out.println("\t\t\tЭто калькулятор суммирования (плюсика^_^)");
+//        System.out.println("Введите первое число:");
+//        double a = new Scanner(System.in).nextDouble();
+//        System.out.println("Введите второе число:");
+//        double b = new Scanner(System.in).nextDouble();
+//        System.out.println(a + " + " + b + " = " + (a+b));
+
+//        System.out.println("\t\t\t Калькулятор вычитания (минусик*_*)");
+//        System.out.println("Введите первое число:");
+//        double a = new Scanner(System.in).nextDouble();
+//        System.out.println("Введите второе число:");
+//        double b = new Scanner(System.in).nextDouble();
+//        System.out.println(a + " - " + b + " = " + (a-b));
+
+//        System.out.println("\t\t\t Калькулятор деления(на 2? да хоть на сколько)");
+//        System.out.println("Введите первое число:");
+//        double a = new Scanner(System.in).nextDouble();
+//        System.out.println("Введите второе число:");
+//        double b = new Scanner(System.in).nextDouble();
+//        System.out.println(a + " / "+ b + " = " + (a/b));
+    }
+}
